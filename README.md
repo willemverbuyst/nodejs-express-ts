f5e88f9ceeefbf85dd2c75d3108424e822d78c00
Willem Verbuyst <willemverbuyst@gmail.com>
2020-08-20 11:02:10 +0200

    Add patch and delete functionality

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## What is this?
 
 Exercise with node, express and ts.
+
 Setting up a server, REST api with TypeScript.
+
 Part of the 'Understanding TypeScript' course by Maximilian Schwarzmüller.
--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -11,3 +11,35 @@ export const createTodo: RequestHandler = (req, res, next) => {
 
   res.status(201).json({ message: 'Created todo', createdTodo: newTodo });
 };
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const { id } = req.params;
+  const updatedText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === id);
+
+  if (todoIndex < 0) {
+    throw new Error('Could not find todo!');
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+  res.json({ message: 'updated!', updatedTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const { id } = req.params;
+  const todoIndex = TODOS.findIndex((todo) => todo.id === id);
+
+  if (todoIndex < 0) {
+    throw new Error('Could not find todo!');
+  }
+
+  TODOS.splice(todoIndex, 1);
+
+  res.json({ message: 'Todo is deleted' });
+};
